a34e9f981d20bd91e38edb257f5ea0c982d6d19d
MJ-DYNA <maximedyna@gmail.com>
2025-11-27 07:36:23 -0500

    Login is now working

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+    TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 )
 from django.contrib.auth.views import LogoutView
 
@@ -10,6 +10,8 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    
     
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
